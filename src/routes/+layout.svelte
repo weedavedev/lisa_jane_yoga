967e92eb4862dbd07455d68a9f0921088fb70e13
weedavedev <admin@wallacedevelopment.co.uk>
2025-02-19 10:53:13 +0100

    Weird git behaviour, re-commit before trying new branchgit add .!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,11 @@
 <script>
-    // import {mediaSource} from '$lib/constants';
     import '../lib/styles/global.css';
-    import {onMount} from 'svelte';
-
     import MobileNavigation from '$lib/components/mobileNavigation.svelte';
-    // let backgroundImage = `${mediaSource}/lisa_background_cropped.jpg`;
-    // let backgroundImageMobile = `${mediaSource}/lisa_background_mobile.jpg`;
-    //console.log(backgroundImage)
-    let isLargeScreen = false;
-
-
-    // Function to check screen size
-    const checkScreenSize = () => {
-        isLargeScreen = window.innerWidth >= 768;
-    };
-
-    // Check on mount and add resize event listener
-    onMount(() => {
-        checkScreenSize(); // Initial check
-        window.addEventListener('resize', checkScreenSize);
-
-        return () => {
-            window.removeEventListener('resize', checkScreenSize);
-        };
-    });
-
 </script>
 
-<div class={`main background_image ${isLargeScreen ? 'bg-[url(/media/lisa_background_cropped.jpg)]' : 'bg-[url(/media/lisa_background_mobile.jpg)]'}`}>
-    <!--    <Navbar/>   -->
-    <div class="holding_page_content ">
+<div class="main background_image">
+    <div class="holding_page_content">
         <slot/>
     </div>
-
     <MobileNavigation />
-    <!--    <Footer/>   -->
 </div>
